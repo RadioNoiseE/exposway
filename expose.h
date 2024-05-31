d95d912459b3f147251119a5d384a05f5ea7a716
RadioNoiseE <j18516785606@icloud.com>
2024-05-31 21:00:40 +0800

    modified default key debounce time

--- expose.h
+++ expose.h
@@ -6,5 +6,7 @@
 	1, 1, 1                     /* color for the title font rendered under the window */
 #define TITLE_SIZE 12               /* size for the font used to render the title */
 #define DISPLAY_GAP 2 * TITLE_SIZE  /* gap between the display edges and the windows */
+#define WIN_GRID_GAP 16             /* minimum amount of gap between windows */
 #define WIN_GRID_FACTOR 0.78        /* factor controlling the gap between window and grid */
-#define DEBOUNCE_DELAY_MS 400       /* key repeat */
+#define WIN_QUAD_FACTOR 0.9         /* factor to scale down on square windows (optimal) */
+#define DEBOUNCE_DELAY_MS 300       /* key repeat */
