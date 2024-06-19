146c9de61df56a58821ae29109fc6f3101f8f13e
RadioNoiseE <j18516785606@icloud.com>
2024-06-19 22:07:15 +0800

    lazy capture, ignore focus

--- collect.sh
+++ collect.sh
@@ -44,7 +44,7 @@ swaymsg -mt subscribe '["window"]' |\
         reference=$(jq -r '.container.name' <<< "$win")
         node=$(jq -r '.container.id' <<< "$win")
         focused=$(jq -r '.container.focused' <<< "$win")
-        refresh=("focus" "fullscreen_mode" "move" "floating" "title") # when urgent or marked we take no measure, when newed the geometry hasn't yet been initialized
+        refresh=("fullscreen_mode" "move" "floating" "title") # when focuse, urgent or marked we take no measure, when newed the geometry hasn't yet been initialized
         delete=("close") # circumstance when snapshot needs to be destroyed
         if [[ "$reference" =~ "Expose Sway" ]]; then
             log "# expose!"
